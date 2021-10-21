e54bfb1f4a0c468e7d7bd13c3a8efef6de45604b
safrana shereen <safranashereen@safranas-MacBook-Pro.local>
2021-10-22 01:27:02 +0530

    init

---
+++ precache-manifest.0b77d11b23b46627d5679e734ab6cb23.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33964039d8c7189b7b0c487ac774956",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d47ad61973f3618e488e",
+    "url": "/static/css/2.40c4fee1.chunk.css"
+  },
+  {
+    "revision": "5fd344aaf9d3e0ebc064",
+    "url": "/static/css/main.6bc0d7a9.chunk.css"
+  },
+  {
+    "revision": "d47ad61973f3618e488e",
+    "url": "/static/js/2.99497393.chunk.js"
+  },
+  {
+    "revision": "5fd344aaf9d3e0ebc064",
+    "url": "/static/js/main.b9bec189.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
